b1967beb05d6a1388d3756f3c2b82053d192f2b9
yannunes11 <yannunes112@gmail.com>
2022-10-19 06:35:42 -0300

    feat: test form submission

--- c-stateful-version/ql13-solution/src/tests/TodoForm.test.js
+++ c-stateful-version/ql13-solution/src/tests/TodoForm.test.js
@@ -10,7 +10,10 @@ jest.mock("../Components/utils/DateCreated", () => {
 });
 
 describe(`TodoForm test suite`, () => {
-
+    let submitTodo;
+    beforeEach(() => {
+        submitTodo = jest.fn();
+    });
     describe(`DateCreated function and render tests`, () => {
         test(`it should render a DateCreated component a date`, () => {
 
@@ -71,6 +74,43 @@ describe(`TodoForm test suite`, () => {
             expect(submitBtn.props.disabled).toBe(false);
             expect(submitBtn.props.className).toContain(`btn-primary`);
 
+        });
+    });
+
+    describe('Fopr submission test', () => {
+        test('it should call submitTodo with a prescribed parameters on submision', async () => {
+            const testRenderer = create(<TodoForm submitTodo={submitTodo}/>);
+            const testInstance = testRenderer.root;
+            const descInput = testInstance.findByProps({name: 'todoDescription'});
+            const completedInput = testInstance.findByProps({name: 'todoCompleted'});
+            const descTestValue = 'Test';
+            const compTestValue = false;
+            const form = testInstance.findByType('form');
+
+            await act(async () => {
+                completedInput.props.onChange({
+                    target: {
+                        checked: compTestValue
+                    }});
+            });
+
+            await act(async () => {
+                descInput.props.onChange({
+                    target: {
+                        value: descTestValue
+                    }
+                })
+            });
+
+            await act(async () => {
+                form.props.onSubmit(new Event('form'));
+            });
+
+            expect(submitTodo).toHaveBeenCalledTimes(1);
+            expect(submitTodo).toHaveBeenCalledWith(descTestValue, null, compTestValue);
+
+            expect(descInput.props.value).toBe('');
+            expect(completedInput.props.checked).toBe(false);
         });
     });
 });
