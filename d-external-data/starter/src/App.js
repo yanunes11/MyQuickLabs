9ec97acb3a563fb0aeb433185aee5ae8bd1fd705
yannunes11 <yannunes112@gmail.com>
2022-10-19 10:18:08 -0300

    feat: getting data not fixed

--- d-external-data/starter/src/App.js
+++ d-external-data/starter/src/App.js
@@ -4,27 +4,49 @@ import 'bootstrap';
 import 'popper.js';
 import 'jquery';
 import './Components/css/qa.css';
-import sampleTodos from './sampleTodos.json';
-
+// import sampleTodos from './sampleTodos.json';
+import { useEffect } from 'react';
+import axios from 'axios';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import AllTodos from './Components/AllTodos';
 import AddEditTodo from './Components/AddEditTodo';
 
+const TODOSURL = 'http://localhost/4000/todos';
+
 function App() {
 
-  const [todos, setTodos] = useState(sampleTodos);
+  const [getError, setGetError] = useState('');
+  const [todos, setTodos] = useState({});
+  useEffect(() => {
+    const getData = async () => {
+      setTodos(await getTodos());
+    }
+    setTimeout(() => {
+      getData();
+    }, 5000)
+  }, []);
+
+  const getTodos = async () => {
+    try {    
+      const res = await axios.get(TODOSURL); 
+      return res.data.lenght > 0? {todos: res.data} : {error: 'there are no todos stored'}
+    } catch (e) {
+      setGetError('Data not available from server: ${e.mesage}');
+      return {error: 'Data not available from server: ${e.mesage}'}
+    }
+  }
 
   const submitTodo = todo => {
     const updatedTodos = [...todos, todo];
-    setTodos(updatedTodos);
+    setTodos({todos: updatedTodos});
   }
 
   return (
     <div className="container">
       <Header />
       <div className="container">
-        <AllTodos data={{ todos }} />
+        <AllTodos data={ todos } />
         <AddEditTodo submitTodo={submitTodo} />
       </div>
       <Footer />
--- d-external-data/starter/src/Components/AllTodos.jsx
+++ d-external-data/starter/src/Components/AllTodos.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './css/AllTodos.css';
-
 import Todo from './Todo';
 import TodoModel from './utils/Todo.model';
+import { useEffect } from 'react';
 
 const AllTodos = ({ data }) => {
-    const todos = data.todos.map(currentTodo => {
-        const todo = new TodoModel(currentTodo.todoDescription, currentTodo.todoDateCreated, currentTodo.todoCompleted, currentTodo._id);
-        return <Todo todo={todo} key={todo._id} />
-    });
+    const [dataStatus, setDataStatus] = useState({name: 'loading', message: 'Data is loading'});
+    useEffect(() => {
+        if (data?.error) {
+            setDataStatus({name: 'error', message: data.error})
+        } else if (data?.todos) {
+            const ds = data.todos.length > 0 ? setDataStatus({name: 'data', message: null}) : setDataStatus({name: 'nodata', message: 'there are no todos saved previously'})
+        } else {
+            setDataStatus({name: 'loading', message: 'Data is loading'});
+        }
+    }, [data]);
+
+    const populatedTable = () => {
+        if (data?.todos?.lenght > 0) {
+            return data.todos.map(currentTodo => {
+                const {todoDescription, todoDateCreated, todoCompleted, _id} = currentTodo;
+                const todo = new TodoModel(todoDescription, todoDateCreated?.toISOString, todoCompleted, _id);
+                return <Todo todo={todo} key={todo._id}/>;
+            });
+        }
+        return (
+            <tr>
+                <td id={dataStatus.name} colSpan="3">{dataStatus.message}</td>
+            </tr>
+        );
+    }
     return (
         <div className="row">
             <h3>Todos List</h3>
@@ -21,23 +42,29 @@ const AllTodos = ({ data }) => {
                         <th>Action</th>
                     </tr>
                 </thead>
-                <tbody>{todos}</tbody>
+                <tbody>{populatedTable()}</tbody>
             </table>
         </div>
     );
 };
 
 AllTodos.propTypes = {
-    data: PropTypes.exact({
-        todos: PropTypes.arrayOf(
-            PropTypes.exact({
-                _id: PropTypes.string,
-                todoDescription: PropTypes.string,
-                todoDateCreated: PropTypes.string,
-                todoCompleted: PropTypes.bool
-            })
-        )
-    })
+    data: PropTypes.oneOfType([
+        PropTypes.exact({
+            todos: PropTypes.arrayOf(
+                PropTypes.exact({
+                    _id: PropTypes.string,
+                    todoDescription: PropTypes.string,
+                    todoDateCreated: PropTypes.string,
+                    todoCompleted: PropTypes.bool
+                })
+            )
+        }),
+        PropTypes.exact({
+            error: PropTypes.string
+        }),
+        PropTypes.exact({})
+    ])
 };
 
 export default AllTodos;
